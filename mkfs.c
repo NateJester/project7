10f79545d6492c28b398e8d086fc9c5d55354f58
Nathaniel Jester <njester@royal-05.cs.wisc.edu>
2024-04-29 22:23:14 -0500

    got mkfs.c passing tests

--- mkfs.c
+++ mkfs.c
@@ -1,10 +1,11 @@
-#include <fuse.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include "wfs.h"
 
-int file;
+char* file_name;
 int num_inodes;
 int num_blocks;
 
@@ -14,19 +15,25 @@ static int parse_args(int argc, char **argv) {
                 switch (op) {
 
                 case 'd':
-                printf("%s", optarg);
+                file_name = optarg;
                 break;
 
                 case 'i':
                 num_inodes = atoi(optarg);
-                printf("%d", num_inodes);
+ 				if (num_inodes % 32 == 0) {
+ 					break;
+ 				}
+ 				num_inodes -= (num_inodes % 32);
+ 				num_inodes += 32;
                 break;
 
                 case 'b':
                 num_blocks = atoi(optarg);
-                num_blocks += 32;
+                if (num_blocks % 32 == 0) {
+                	break;
+                }
                 num_blocks -= (num_blocks % 32);
-				printf("%d", num_blocks);
+                num_blocks += 32;
                 break;
 
                 default:
@@ -41,4 +48,42 @@ int main(int argc, char** argv) {
 	if (parse_args(argc, argv) != 0) {
 		exit(EXIT_FAILURE);
 	}
+
+	printf("%s", file_name);
+	FILE* fp = fopen(file_name, "r+");
+	if (fp == NULL) {
+		perror("fopen");
+		return 1;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	int file_size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	int size_needed = (num_blocks + 1) * BLOCK_SIZE;
+	if (file_size < size_needed) {
+		printf("Disk image file to small to accommodate number of blocks");
+		return 1;
+	}
+
+	struct wfs_inode root;
+	root.num = 0;
+	root.nlinks = 1;
+	
+	struct wfs_sb superblock;
+	superblock.num_inodes = num_inodes;
+	superblock.num_data_blocks = num_blocks;
+	superblock.i_bitmap_ptr = sizeof(superblock);
+	superblock.d_bitmap_ptr = sizeof(superblock) + num_inodes / 8;
+	superblock.i_blocks_ptr = superblock.d_bitmap_ptr + num_blocks / 8;
+	superblock.d_blocks_ptr = superblock.i_blocks_ptr + num_inodes * 0x200;
+	
+	fwrite(&superblock, sizeof(struct wfs_sb), 1, fp);
+
+	fseek(fp, superblock.i_blocks_ptr, SEEK_SET);
+	fwrite(&root, sizeof(struct wfs_inode), 1, fp);
+
+	char* i_bitmap = calloc(num_inodes / 8, 1);
+	i_bitmap[0] = 1;
+	fseek(fp, superblock.i_bitmap_ptr, SEEK_SET);
+	fwrite(i_bitmap, sizeof(i_bitmap), 1, fp);
 }
